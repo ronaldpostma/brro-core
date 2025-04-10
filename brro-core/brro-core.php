7d8733e211b81117cdc48973e8b8d2a96ff8cf41
ronaldpostma <admin@ronaldpostma.com>
2025-04-10 12:06:55 +0200

    Author

--- brro-core/brro-core.php
+++ brro-core/brro-core.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/ronaldpostma/brro-core
  * Description: Global core functions for all sites developed by Brro and development tools used within Elementor and the frontend.
  * Version: 1.5.9
- * Author: Ronald Postma 
+ * Author: Ronald Postma @ Brro.nl
  * Author URI: https://brro.nl/
  * Plugin Icon: https://example.com/path-to-your-icon.png
  * 
