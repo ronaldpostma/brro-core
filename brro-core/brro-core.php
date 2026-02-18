fdaa532d007f0bd07a63865efc4756b64eccf2f8
ronaldpostma <117193388+ronaldpostma@users.noreply.github.com>
2026-02-18 10:23:10 +0100

    Update brro-core.php

--- brro-core/brro-core.php
+++ brro-core/brro-core.php
@@ -3,7 +3,7 @@
  * Plugin Name: Brro Core
  * Plugin URI: https://github.com/ronaldpostma/brro-core
  * Description: Global core functions and development tools for sites developed by Brro.
- * Version: 2.0.5
+ * Version: 2.1.0
  * Author: Ronald Postma @ Brro.nl
  * Author URI: https://brro.nl/
  * License: GPLv2 or later
